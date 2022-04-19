521235f083e84fe07e2ec4e4ab0a356ef550522e
Emanuel Figueiredo de Aguiar <101213551+Emanuelfaguiar@users.noreply.github.com>
2022-04-19 15:12:08 -0300

    BaSP-W05-2022: Made for exercises

--- semana-05/exercises/05_for.js
+++ semana-05/exercises/05_for.js
@@ -1,4 +1,56 @@
-// A) Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras.
-// B) Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada.
-// C) Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena completa.
-// D) Crear una array vacío y con un bucle for de 10 repeticiones. Llenar el array con el número de la repetición, es decir que al final de la ejecución del bucle for debería haber 10 elementos dentro del array, desde el número 0 hasta al número 9. Mostrar por la consola del navegador el array final (utilizar console.log).
+console.log('05-For');
+var names = ['maria', 'pablo', 'juan', 'emanuel', 'alfonso'];
+console.log('Array: ' + names);
+
+/* A) Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de JavaScript para mostrar 
+una alerta utilizando cada una de las palabras.*/
+console.log('Exercise A');
+
+for (let i=0; i < names.length; i++){
+    alert(names[i]);
+}
+
+/* B) Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra 
+modificada.*/
+console.log('Exercise B');
+
+var upperName = [];
+var lowerName = [];
+
+for (let i=0; i < names.length; i++){
+    upperName[i] = names [i].substring(0,1).toUpperCase();
+    lowerName[i] = names[i].substring(1,).toLowerCase();
+    upperName[i] += lowerName[i];
+    alert(upperName[i]);
+}
+
+console.log('Array with Uppercase: ' + upperName);
+
+/* C) Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a) recorrerlo con un 
+bucle for para ir guardando cada palabra dentro de la variable sentence. Al final mostrar una única alerta con la cadena
+completa.*/
+console.log('Exercise C');
+
+var sentence = '';
+
+for (let i=0; i< names.length; i++){
+    sentence += ' ' + names[i];
+    if (names.toString().length <= sentence.length){
+        alert('Sentence: ' + sentence);
+    }
+}
+
+console.log('Complete sentence: ' + sentence);
+
+/* D) Crear una array vacío y con un bucle for de 10 repeticiones. Llenar el array con el número de la repetición, es
+decir que al final de la ejecución del bucle for debería haber 10 elementos dentro del array, desde el número 0 hasta al
+número 9. Mostrar por la consola del navegador el array final (utilizar console.log).*/
+console.log('Exercise D');
+
+var empty = [];
+
+for (let i=0; i < 10; i++){
+    empty.push(i);
+}
+
+console.log('Array with 10 bucles: ' + empty);
